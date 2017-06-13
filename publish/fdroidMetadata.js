6dfc3b1e99ee35dffb59469473a4c6a23cac64ce
DivDE <divde@free.fr>
2017-06-14 00:33:03 +0200

    feat: publish to an F-Droid repository

---
+++ publish/fdroidMetadata.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+async function readProperty(...pathParts) {
+  const fileContent = await readFile(path.join(__dirname, 'play', ...pathParts), 'utf-8');
+  return fileContent.trim();
+}
+
+async function writeMetadata() {
+  const language = await readProperty('defaultLanguage');
+
+  const metadata = {
+    "License": "AGPL",
+    "Web Site": await readProperty('contactWebsite'),
+    "Source Code": "https://github.com/musicociel/webapp",
+    "Issue Tracker": "https://github.com/musicociel/webapp/issues",
+    "Name": await readProperty(language, 'listing', 'title'),
+    "Summary": await readProperty(language, 'listing', 'shortdescription'),
+    "Description": await readProperty(language, 'listing', 'fulldescription')
+  };
+
+  await writeFile(path.join(process.env.FDROID_REPO, 'metadata', 'fr.musicociel.json'), JSON.stringify(metadata));
+}
+
+writeMetadata().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
--- publish/publish.sh
+++ publish/publish.sh
@@ -10,8 +10,16 @@ if ! npm run semantic-release pre ; then
   exit 0
 fi
 
-echoTask 'openssl aes-256-cbc -K $encrypted_dfa60c7f1074_key -iv $encrypted_dfa60c7f1074_iv -in publish/.publish.tar.bz2.enc -out publish/.publish.tar.bz2 -d' &&
-openssl aes-256-cbc -K $encrypted_dfa60c7f1074_key -iv $encrypted_dfa60c7f1074_iv -in publish/.publish.tar.bz2.enc -out publish/.publish.tar.bz2 -d &&
+export APK_VERSION="$(node -e 'console.log(require("./package.json").version)')"
+export FDROID_SERVER="$PWD/publish/fdroidserver"
+export FDROID_REPO="$PWD/publish/fdroid"
+export jks_file="$PWD/publish/keystore.jks"
+
+echoTask 'pip install fdroidserver --root "$FDROID_SERVER"' &&
+pip install fdroidserver --root "$FDROID_SERVER" &&
+
+echoTask 'openssl aes-256-cbc -K $encrypted_2c96595a2e49_key -iv $encrypted_2c96595a2e49_iv -in publish/.publish.tar.bz2.enc -out publish/.publish.tar.bz2 -d' &&
+openssl aes-256-cbc -K $encrypted_2c96595a2e49_key -iv $encrypted_2c96595a2e49_iv -in publish/.publish.tar.bz2.enc -out publish/.publish.tar.bz2 -d &&
 
 echoTask 'tar -C publish -jxf publish/.publish.tar.bz2' &&
 tar -C publish -jxf publish/.publish.tar.bz2 &&
@@ -26,10 +34,33 @@ echoTask 'patch -d platforms/android < publish/build.gradle.diff' &&
 patch -d platforms/android < publish/build.gradle.diff &&
 
 echoTask './node_modules/.bin/cordova build android --release -- --keystore=$PWD/publish/keystore.jks --password=$jks_password --storePassword=$jks_password --alias=uploadkey' &&
-./node_modules/.bin/cordova build android --release -- --keystore=$PWD/publish/keystore.jks --password=$jks_password --storePassword=$jks_password --alias=uploadkey &&
+./node_modules/.bin/cordova build android --release -- --keystore=$jks_file --password=$jks_password --storePassword=$jks_password --alias=uploadkey &&
+
+echoTask 'git clone --depth 1 "https://$GH_TOKEN@github.com/musicociel/fdroid.git" "$FDROID_REPO"' &&
+git clone --depth 1 "https://$GH_TOKEN@github.com/musicociel/fdroid.git" "$FDROID_REPO" &&
+
+echoTask 'node publish/fdroidMetadata.js' &&
+node publish/fdroidMetadata.js &&
+
+echoTask 'cp "$PWD/platforms/android/build/outputs/apk/android-release.apk" "publish/fdroid/repo/musicociel-$APK_VERSION.apk"' &&
+cp "$PWD/platforms/android/build/outputs/apk/android-release.apk" "publish/fdroid/repo/musicociel-$APK_VERSION.apk" &&
+
+echoTask 'cd "$FDROID_REPO" && "$FDROID_SERVER/usr/bin/fdroid" update' &&
+( cd "$FDROID_REPO" && "$FDROID_SERVER/usr/bin/fdroid" update ) &&
+
+echoTask 'cd "$FDROID_REPO" && git config user.name "Release Bot" && git config user.email "divde@musicociel.fr"' &&
+( cd "$FDROID_REPO" && git config user.name "Release Bot" && git config user.email "divde@musicociel.fr" ) &&
+
+echoTask 'cd "$FDROID_REPO" && git add -A && git commit -m "Version $APK_VERSION"' &&
+( cd "$FDROID_REPO" && git add -A && git commit -m "Version $APK_VERSION" ) &&
+
+echoTask 'cd "$FDROID_REPO" && git push origin master' &&
+( cd "$FDROID_REPO" && git push origin master &> /dev/null ) &&
 
 echoTask 'npm publish' &&
 npm publish &&
 
 echoTask 'npm run semantic-release post' &&
-npm run semantic-release post
+npm run semantic-release post &&
+
+true
