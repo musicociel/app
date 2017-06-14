ceb73579ee28deda073990c9669d496528b9a5cc
DivDE <divde@free.fr>
2017-06-14 23:34:42 +0200

    WIP

--- publish/publish.sh
+++ publish/publish.sh
@@ -5,10 +5,11 @@ function echoTask() {
   echo "> $@"
 }
 
-if ! npm run semantic-release pre ; then
-  echo "Not publishing a new release."
-  exit 0
-fi
+echo pip
+pip --version
+
+echo pip3
+pip3 --version
 
 export APK_VERSION="$(node -e 'console.log(require("./package.json").version)')"
 export FDROID_SERVER="$PWD/publish/fdroidserver"
@@ -24,29 +25,14 @@ openssl aes-256-cbc -K $encrypted_2c96595a2e49_key -iv $encrypted_2c96595a2e49_i
 echoTask 'tar -C publish -jxf publish/.publish.tar.bz2' &&
 tar -C publish -jxf publish/.publish.tar.bz2 &&
 
-echoTask 'mkdir platforms/android/src/main' &&
-mkdir platforms/android/src/main &&
-
-echoTask 'ln -s ../../../../publish/play platforms/android/src/main/play' &&
-ln -s ../../../../publish/play platforms/android/src/main/play &&
-
-echoTask 'patch -d platforms/android < publish/build.gradle.diff' &&
-patch -d platforms/android < publish/build.gradle.diff &&
-
-echoTask './node_modules/.bin/cordova build android --release -- --keystore=$PWD/publish/keystore.jks --password=$jks_password --storePassword=$jks_password --alias=uploadkey' &&
-./node_modules/.bin/cordova build android --release -- --keystore=$jks_file --password=$jks_password --storePassword=$jks_password --alias=uploadkey &&
-
 echoTask 'git clone --depth 1 "https://$GH_TOKEN@github.com/musicociel/fdroid.git" "$FDROID_REPO"' &&
 git clone --depth 1 "https://$GH_TOKEN@github.com/musicociel/fdroid.git" "$FDROID_REPO" &&
 
 echoTask 'node publish/fdroidMetadata.js' &&
 node publish/fdroidMetadata.js &&
 
-echoTask 'cp "$PWD/platforms/android/build/outputs/apk/android-release.apk" "publish/fdroid/repo/musicociel-$APK_VERSION.apk"' &&
-cp "$PWD/platforms/android/build/outputs/apk/android-release.apk" "publish/fdroid/repo/musicociel-$APK_VERSION.apk" &&
-
-echoTask 'npm publish' &&
-npm publish &&
+echoTask 'cp "$PWD/platforms/android/build/outputs/apk/android-debug.apk" "publish/fdroid/repo/musicociel-$APK_VERSION.apk"' &&
+cp "$PWD/platforms/android/build/outputs/apk/android-debug.apk" "publish/fdroid/repo/musicociel-$APK_VERSION.apk" &&
 
 echoTask 'cd "$FDROID_REPO" && "$FDROID_SERVER/usr/local/bin/fdroid" update' &&
 ( cd "$FDROID_REPO" && "$FDROID_SERVER/usr/local/bin/fdroid" update ) &&
@@ -56,11 +42,5 @@ echoTask 'cd "$FDROID_REPO" && git config user.name "Release Bot" && git config 
 
 echoTask 'cd "$FDROID_REPO" && git add -A && git commit -m "Version $APK_VERSION"' &&
 ( cd "$FDROID_REPO" && git add -A && git commit -m "Version $APK_VERSION" ) &&
-
-echoTask 'cd "$FDROID_REPO" && git push origin master' &&
-( cd "$FDROID_REPO" && git push origin master &> /dev/null ) &&
-
-echoTask 'npm run semantic-release post' &&
-npm run semantic-release post &&
 
 true
