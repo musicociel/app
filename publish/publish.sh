466fb5f284dcdd5b8ab34c960ec3935f77d29f69
DivDE <divde@free.fr>
2017-06-17 07:56:16 +0200

    WIP

--- publish/publish.sh
+++ publish/publish.sh
@@ -5,19 +5,28 @@ function echoTask() {
   echo "> $@"
 }
 
-echo pip
-pip --version
-
-echo pip3
-pip3 --version
-
 export APK_VERSION="$(node -e 'console.log(require("./package.json").version)')"
 export FDROID_SERVER="$PWD/publish/fdroidserver"
 export FDROID_REPO="$PWD/publish/fdroid"
 export jks_file="$PWD/publish/keystore.jks"
 
-echoTask 'pip install fdroidserver --root "$FDROID_SERVER"' &&
-pip install fdroidserver --root "$FDROID_SERVER" &&
+echoTask 'curl -O https://s3.amazonaws.com/travis-python-archives/binaries/ubuntu/12.04/x86_64/python-3.6.tar.bz2' &&
+curl -O https://s3.amazonaws.com/travis-python-archives/binaries/ubuntu/12.04/x86_64/python-3.6.tar.bz2 &&
+
+echoTask 'sudo tar xjf python-3.6.tar.bz2 --directory /' &&
+sudo tar xjf python-3.6.tar.bz2 --directory / &&
+
+echoTask 'source ~/virtualenv/python3.6/bin/activate' &&
+source ~/virtualenv/python3.6/bin/activate &&
+
+echoTask 'python --version' &&
+python --version &&
+
+echoTask 'pip --version' &&
+pip --version
+
+echoTask 'pip install fdroidserver' &&
+pip install fdroidserver &&
 
 echoTask 'openssl aes-256-cbc -K $encrypted_2c96595a2e49_key -iv $encrypted_2c96595a2e49_iv -in publish/.publish.tar.bz2.enc -out publish/.publish.tar.bz2 -d' &&
 openssl aes-256-cbc -K $encrypted_2c96595a2e49_key -iv $encrypted_2c96595a2e49_iv -in publish/.publish.tar.bz2.enc -out publish/.publish.tar.bz2 -d &&
@@ -34,8 +43,8 @@ node publish/fdroidMetadata.js &&
 echoTask 'cp "$PWD/platforms/android/build/outputs/apk/android-debug.apk" "publish/fdroid/repo/musicociel-$APK_VERSION.apk"' &&
 cp "$PWD/platforms/android/build/outputs/apk/android-debug.apk" "publish/fdroid/repo/musicociel-$APK_VERSION.apk" &&
 
-echoTask 'cd "$FDROID_REPO" && "$FDROID_SERVER/usr/local/bin/fdroid" update' &&
-( cd "$FDROID_REPO" && "$FDROID_SERVER/usr/local/bin/fdroid" update ) &&
+echoTask 'cd "$FDROID_REPO" && fdroid update' &&
+( cd "$FDROID_REPO" && fdroid update ) &&
 
 echoTask 'cd "$FDROID_REPO" && git config user.name "Release Bot" && git config user.email "divde@musicociel.fr"' &&
 ( cd "$FDROID_REPO" && git config user.name "Release Bot" && git config user.email "divde@musicociel.fr" ) &&
